0de75cc0ee31fb0301ae4206f7f3f57a8c513bae
obsidianRuby <obsidian@ruby.exchange>
2022-03-17 10:38:44 +0100

    feat: RubyMaker optimisations (WIP)

--- contracts/RubyMaker.sol
+++ contracts/RubyMaker.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.12;
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
 import "./token_mappings/RubyToken.sol";
 
 import "./libraries/SafeERC20.sol";
@@ -11,23 +13,19 @@ import "./amm/interfaces/IUniswapV2Pair.sol";
 import "./amm/interfaces/IUniswapV2Factory.sol";
 import "./interfaces/IRubyStaker.sol";
 
-import "./Ownable.sol";
 
-// RubyMaker is fork of SushiMaker
-contract RubyMaker is Ownable {
+contract RubyMaker is OwnableUpgradeable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IUniswapV2Factory public immutable factory;
-    IRubyStaker public immutable rubyStaker;
-    address private immutable ruby;
-    address private immutable ethc;
+    IUniswapV2Factory public factory;
+    IRubyStaker public rubyStaker;
+    address public ruby;
+    address public usdToken; // USD token (USDP initially)
+
     uint256 public burnPercent;
 
-    mapping(address => address) internal _bridges;
-
-    event LogBridgeSet(address indexed token, address indexed bridge);
-    event LogConvert(
+    event Convert(
         address indexed server,
         address indexed token0,
         address indexed token1,
@@ -37,26 +35,41 @@ contract RubyMaker is Ownable {
         uint256 amountRubyBurned
     );
 
-    event BurnPercentChanged(uint256 newBurnPercent);
+    event BurnPercentSet(uint256 newBurnPercent);
 
-    constructor(
-        address _factory,
-        address _rubyStaker,
-        address _ruby,
-        address _ethc,
+    event RubyTokenSet(address ruby);
+
+    event UsdTokenSet(address usdToken);
+
+    event AmmFactorySet(address factory);
+
+    event RubyStakerSet(address rubyStaker);
+
+    event PairWithdrawn(address indexed pair, uint256 amountWithdrawn);
+
+    function initialize(
+        address _owner,
+        address _factory, 
+        address _rubyStaker, 
+        address _rubyToken, 
+        address _usdToken,
         uint256 _burnPercent
-    ) public {
-        require(_factory != address(0), "RubyMaker: Invalid factory address.");
+        ) external initializer() { 
+        require(_owner != address(0), "RubyMaker: Invalid owner address");
+        require(_factory != address(0), "RubyMaker: Invalid AMM factory address.");
         require(_rubyStaker != address(0), "RubyMaker: Invalid rubyStaker address.");
-        require(_ruby != address(0), "RubyMaker: Invalid ruby address.");
-        require(_ethc != address(0), "RubyMaker: Invalid ethc address.");
+        require(_rubyToken != address(0), "RubyMaker: Invalid ruby address.");
+        require(_usdToken != address(0), "RubyMaker: Invalid USD token address.");
         require(_burnPercent >= 0 && _burnPercent <= 100, "RubyMaker: Invalid burn percent.");
 
+        OwnableUpgradeable.__Ownable_init();
+        transferOwnership(_owner);
+    
         factory = IUniswapV2Factory(_factory);
         rubyStaker = IRubyStaker(_rubyStaker);
-        ruby = _ruby;
-        IERC20(_ruby).approve(_rubyStaker, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
-        ethc = _ethc;
+        ruby = _rubyToken;
+        IERC20(_rubyToken).approve(_rubyStaker, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+        usdToken = _usdToken;
 
         // Note: Percentages are defined with 3 decimals (20% is defined as 20)
         // 0.05% (1/6th) of the total fees (0.30%) are sent to the RubyMaker
@@ -68,23 +81,7 @@ contract RubyMaker is Ownable {
     function setBurnPercent(uint256 newBurnPercent) external onlyOwner {
         require(newBurnPercent >= 0 && newBurnPercent <= 100, "RubyMaker: Invalid burn percent.");
         burnPercent = newBurnPercent;
-        emit BurnPercentChanged(newBurnPercent);
-    }
-
-    function bridgeFor(address token) public view returns (address bridge) {
-        bridge = _bridges[token];
-        if (bridge == address(0)) {
-            bridge = ethc;
-        }
-    }
-
-    function setBridge(address token, address bridge) external onlyOwner {
-        // Checks
-        require(token != ruby && token != ethc && token != bridge, "RubyMaker: Invalid bridge");
-
-        // Effects
-        _bridges[token] = bridge;
-        emit LogBridgeSet(token, bridge);
+        emit BurnPercentSet(newBurnPercent);
     }
 
     modifier onlyEOA() {
@@ -106,6 +103,10 @@ contract RubyMaker is Ownable {
     }
 
     function _convert(address token0, address token1) internal {
+        // We only support pairs where the one of the token is usdToken or ruby
+        // when this changes, this needs to be modified along with the _convertStep function
+        require((token0 == usdToken || token0 == ruby) && (token1 == usdToken || token1 == ruby),
+         "RubyMaker: Conversion unsupported");
         // Interactions
         IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(token0, token1));
         require(address(pair) != address(0), "RubyMaker: Invalid pair");
@@ -126,7 +127,7 @@ contract RubyMaker is Ownable {
 
         rubyStaker.notifyRewardAmount(1, rubyRewards);
 
-        emit LogConvert(msg.sender, token0, token1, amount0, amount1, rubyRewards, rubyToBurn);
+        emit Convert(msg.sender, token0, token1, amount0, amount1, rubyRewards, rubyToBurn);
     }
 
     function _convertStep(
@@ -140,43 +141,23 @@ contract RubyMaker is Ownable {
             uint256 amount = amount0.add(amount1);
             if (token0 == ruby) {
                 rubyOut = amount;
-            } else if (token0 == ethc) {
-                rubyOut = _toRUBY(ethc, amount);
             } else {
-                address bridge = bridgeFor(token0);
-                amount = _swap(token0, bridge, amount, address(this));
-                rubyOut = _convertStep(bridge, bridge, amount, 0);
+                // token0 == usdToken
+                rubyOut = _toRUBY(usdToken, amount);
             }
         } else if (token0 == ruby) {
-            // eg. RUBY - ETH
+            // eg. RUBY - USDP
             rubyOut = _toRUBY(token1, amount1).add(amount0);
         } else if (token1 == ruby) {
-            // eg. USDT - RUBY
+            // eg. USDP - RUBY
             rubyOut = _toRUBY(token0, amount0).add(amount1);
-        } else if (token0 == ethc) {
-            // eg. ETH - USDC
-            rubyOut = _toRUBY(ethc, _swap(token1, ethc, amount1, address(this)).add(amount0));
-        } else if (token1 == ethc) {
-            // eg. USDT - ETH
-            rubyOut = _toRUBY(ethc, _swap(token0, ethc, amount0, address(this)).add(amount1));
+        } else if (token0 == usdToken) {
+            // eg. USDP - XYZ
+            rubyOut = _toRUBY(usdToken, _swap(token1, usdToken, amount1, address(this)).add(amount0));
         } else {
-            // eg. MIC - USDT
-            address bridge0 = bridgeFor(token0);
-            address bridge1 = bridgeFor(token1);
-            if (bridge0 == token1) {
-                // eg. MIC - USDT - and bridgeFor(MIC) = USDT
-                rubyOut = _convertStep(bridge0, token1, _swap(token0, bridge0, amount0, address(this)), amount1);
-            } else if (bridge1 == token0) {
-                // eg. WBTC - DSD - and bridgeFor(DSD) = WBTC
-                rubyOut = _convertStep(token0, bridge1, amount0, _swap(token1, bridge1, amount1, address(this)));
-            } else {
-                rubyOut = _convertStep(
-                    bridge0,
-                    bridge1, // eg. USDT - DSD - and bridgeFor(DSD) = WBTC
-                    _swap(token0, bridge0, amount0, address(this)),
-                    _swap(token1, bridge1, amount1, address(this))
-                );
-            }
+            // token1 == usdToken
+            // eg. XYZ - USDP
+            rubyOut = _toRUBY(usdToken, _swap(token0, usdToken, amount0, address(this)).add(amount1));
         }
     }
 
@@ -207,4 +188,41 @@ contract RubyMaker is Ownable {
     function _toRUBY(address token, uint256 amountIn) internal returns (uint256 amountOut) {
         amountOut = _swap(token, ruby, amountIn, address(this));
     }
+
+
+    // ADMIN functions
+
+    function setRubyToken(address newRuby) external onlyOwner {
+        require(newRuby != address(0), "RubyMaker: Invalid ruby address.");
+        ruby = newRuby;
+        emit RubyTokenSet(newRuby);
+    }
+
+
+    function setUsdToken(address newUsdToken) external onlyOwner {
+        require(newUsdToken != address(0), "RubyMaker: Invalid USD token address.");
+        usdToken = newUsdToken;
+        emit RubyTokenSet(newUsdToken);
+    }
+
+    function setRubyStaker(address newRubyStaker) external onlyOwner {
+        require(newRubyStaker != address(0), "RubyMaker: Invalid rubyStaker address.");
+        rubyStaker = IRubyStaker(newRubyStaker);
+        emit RubyStakerSet(newRubyStaker);
+    }
+
+    function setAmmFactory(address newFactory) external onlyOwner {
+        require(newFactory != address(0), "RubyMaker: Invalid AMM factory address.");
+        factory = IUniswapV2Factory(newFactory);
+        emit AmmFactorySet(newFactory);
+    }
+
+    function withdrawLP(address pair) external onlyOwner {
+        require(pair != address(0), "RubyMaker: Invalid pair address.");
+        IERC20 _pair = IERC20(pair);
+        uint256 pairBalance = _pair.balanceOf(address(this));
+        _pair.safeTransfer(owner(), pairBalance);
+        emit PairWithdrawn(pair, pairBalance);
+    }
+
 }
