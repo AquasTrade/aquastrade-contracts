9896880cfd2d07ca802a4133c8e14780547235db
jasper <jasper@ruby.exchange>
2022-04-28 18:04:25 +0100

    dont deploy skale specific RNG to localhost

--- deploy/lottery/RandomNumberGenerator.ts
+++ deploy/lottery/RandomNumberGenerator.ts
@@ -2,16 +2,25 @@ import type { HardhatRuntimeEnvironment } from "hardhat/types";
 import type { DeployFunction } from "hardhat-deploy/types";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, network } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy("RNG_Skale", {
-    from: deployer,
-    args: [],
-    log: true,
-    deterministicDeployment: false,
-  });
+  if ((network.name === "hardhat") || (network.name === "localhost")) {
+    await deploy("RNG_Test", {
+      from: deployer,
+      args: [],
+      log: true,
+      deterministicDeployment: false,
+    });
+  } else {
+    await deploy("RNG_Skale", {
+      from: deployer,
+      args: [],
+      log: true,
+      deterministicDeployment: false,
+    });
+  }
 };
 
 func.tags = ["RandomNumberGenerator", "Lottery"];
