affb4cac69821defb55785767e5d6af00f026fe4
jasper <jasper@ruby.exchange>
2022-05-21 00:00:04 +0100

    add capped faucet for europa

--- deploy/faucet/Faucet.ts
+++ deploy/faucet/Faucet.ts
@@ -2,22 +2,30 @@ import type { HardhatRuntimeEnvironment } from "hardhat/types";
 import type { DeployFunction } from "hardhat-deploy/types";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, network } = hre;
   const { deploy, get } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const ruby = (await get("RubyToken")).address;
-  const usdp = (await get("RubyUSDP")).address;
-  const usdc = (await get("RubyUSDC")).address;
-  const usdt = (await get("RubyUSDT")).address;
-  const dai = (await get("RubyDAI")).address;
+  if (network.name === "hardhat") {
+    await deploy("FaucetRubyEuropa", {
+      from: deployer,
+      log: true,
+      deterministicDeployment: false,
+    });
+  } else {
+    const ruby = (await get("RubyToken")).address;
+    const usdp = (await get("RubyUSDP")).address;
+    const usdc = (await get("RubyUSDC")).address;
+    const usdt = (await get("RubyUSDT")).address;
+    const dai = (await get("RubyDAI")).address;
 
-  await deploy("Faucet", {
-    from: deployer,
-    args: [ruby, usdp, usdc, usdt, dai],
-    log: true,
-    deterministicDeployment: false,
-  });
+    await deploy("Faucet", {
+      from: deployer,
+      args: [ruby, usdp, usdc, usdt, dai],
+      log: true,
+      deterministicDeployment: false,
+    });
+  }
 };
 
 func.tags = ["Faucet"];
