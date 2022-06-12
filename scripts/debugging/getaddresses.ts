a9bf77cd1841b31f45fbced0465ec88a45dda4d8
jasper <jasper@ruby.exchange>
2022-06-12 20:45:41 +0100

    fix debug addresses script on l1

--- scripts/debugging/getaddresses.ts
+++ scripts/debugging/getaddresses.ts
@@ -5,17 +5,18 @@ const main = async (hre: HardhatRuntimeEnvironment) => {
   const network = hre.network;
   let accounts = await hre.getNamedAccounts();
 
-  accounts['RubyMasterChef'] = require(`../../deployments/${network.name}/RubyMasterChef.json`).address;
-
   if (network.name === "rubyNewChain") {
     accounts['Faucet'] = require(`../../deployments/${network.name}/Faucet.json`).address;
   } else if (network.name === "europa") {
     accounts['FaucetRubyEuropa'] = require(`../../deployments/${network.name}/FaucetRubyEuropa.json`).address;
   }
 
-  accounts['RubyMaker'] = require(`../../deployments/${network.name}/RubyMaker.json`).address;
-  accounts['RubyStaker'] = require(`../../deployments/${network.name}/RubyStaker.json`).address;
-  accounts['LotteryBurner'] = require(`../../deployments/${network.name}/LotteryBurner.json`).address;
+  if (network.name === "rubyNewChain" || network.name === "europa") {
+    accounts['RubyMasterChef'] = require(`../../deployments/${network.name}/RubyMasterChef.json`).address;
+    accounts['RubyMaker'] = require(`../../deployments/${network.name}/RubyMaker.json`).address;
+    accounts['RubyStaker'] = require(`../../deployments/${network.name}/RubyStaker.json`).address;
+    accounts['LotteryBurner'] = require(`../../deployments/${network.name}/LotteryBurner.json`).address;
+  }
 
   console.log(accounts);
 };
