21693b43cd54e7071a30cb016d0c201dae5a7af1
jasper <jasper@ruby.exchange>
2023-01-08 17:13:02 +0000

    scripts: util script for printing deployment compiler versions
    just checking the necessity of adding compiler 0.7.6 to the
    listed HH versions in 2468c4fb6b0b8feabc8fdb276bbde8ee9c540619

---
+++ scripts/compilerversions.ts
@@ -0,0 +1,42 @@
+// print the compiler version used in all deployments
+//
+//  $ npx hardhat run --network europa scripts/compilerversions.ts
+
+import fs from "fs";
+import path from 'path';
+import { network } from "hardhat";
+
+
+const getCompilerVersion = (jsonObject: any) => {
+  try {
+    return JSON.parse(jsonObject.metadata).compiler.version;
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      return 'unknown';
+    } else {
+      throw error;
+    }
+  }
+}
+
+const readJsonFiles = (directory: string, ignore: string[]) => {
+  const files = fs.readdirSync(directory);
+  let jsonFiles = files.filter((file) => file.endsWith('.json'));
+  ignore.forEach((suffix) => {
+    jsonFiles = jsonFiles.filter((file) => !file.endsWith(suffix));
+  });
+  return jsonFiles.map((file) => {
+    const fileContent = fs.readFileSync(`${directory}/${file}`, 'utf-8');
+    return { file, version: getCompilerVersion(JSON.parse(fileContent)) };
+  });
+}
+
+const main = () => {
+  const directory = path.join(__dirname, '..', 'deployments', `${network.name}`)
+  const jsonObjects = readJsonFiles(directory, ['_Proxy.json', '_Implementation.json']);
+  jsonObjects.forEach((jsonObject) => {
+    console.log(`${jsonObject.file}: ${jsonObject.version}`);
+  });
+}
+
+main();
