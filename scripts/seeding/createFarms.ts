25063df9f909a5fa1d3151d35cfe712b6b8e3565
jasper <jasper@ruby.exchange>
2023-03-09 08:39:13 +0000

    partner farm creation script

--- scripts/seeding/createFarms.ts
+++ scripts/seeding/createFarms.ts
@@ -3,24 +3,8 @@ import { ethers, network } from "hardhat";
 import { RubyMasterChef } from "../../typechain";
 
 import { debugChefPools } from "../utils";
+import { addDoubleRewardFarm, addSingleRewardFarm } from "./utils";
 
-const addDoubleRewardFarm = async (masterChef: RubyMasterChef, lpTokenAddr: string, allocPoints: number, rewarderAddr: string) => {
-  const res = await masterChef.add(allocPoints, lpTokenAddr, rewarderAddr);
-  const receipt = await res.wait(1);
-
-  console.log(`Adding farm for LP token ${lpTokenAddr} with secondary rewarder ${rewarderAddr}`)
-
-  if (receipt.status) {
-    console.log('Adding to RubyMasterChef successful');
-  } else {
-    console.log('Adding to RubyMasterChef failed');
-  }
-};
-
-const addSingleRewardFarm = async (masterChef: RubyMasterChef, lpTokenAddr: string, allocPoints: number) => {
-  const zeroAddress = "0x0000000000000000000000000000000000000000";
-  await addDoubleRewardFarm(masterChef, lpTokenAddr, allocPoints, zeroAddress);
-};
 
 const main = async () => {
   if (network.name !== "europa") {
