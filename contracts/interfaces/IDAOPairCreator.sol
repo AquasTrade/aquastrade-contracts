85c3a0d6649ba79a2c7defd0bdfba96127f4f954
hobbydev71 <kamhongro@gmail.com>
2023-05-04 01:46:44 +0900

    add addLiquidity function to daoPairCreator

--- contracts/interfaces/IDAOPairCreator.sol
+++ contracts/interfaces/IDAOPairCreator.sol
@@ -3,7 +3,6 @@
 pragma solidity >=0.5.0;
 
 interface IDAOPairCreator {
-
     function createPair(address tokenA, address tokenB) external returns (address pair);
 
     function setAdmin(address newAdmin) external;
@@ -11,4 +10,21 @@ interface IDAOPairCreator {
     function setMinimumBalanceRequired(uint256 x) external;
 
     function authorized(address user) external view returns (bool);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
 }
